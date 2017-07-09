a272b156fb79bb76f265c84075d741f2bd1837a0
Eric Pires <ericpires9@gmail.com>
2017-07-09 02:43:58 +0000

    Added encryption module with FROUND and FSb, and external RoundKey access

---
+++ include/systemc/aes_encrypt.h
@@ -0,0 +1,54 @@
+#include <systemc>
+#include "aes_fround.h"
+#include "aes_fsb.h"
+
+enum aes_state {
+  st_wait, st_read, st_addroundkey, st_round_odd, st_round_even, st_round_last, st_subbytes, st_end
+};
+
+SC_MODULE(AES_encrypt)
+{
+  sc_core::sc_in<bool> clock, start, clear;
+  sc_core::sc_in<uint32_t> rk0, rk1, rk2, rk3;
+  sc_core::sc_in<unsigned char[16]> input;
+  sc_core::sc_out<unsigned char[16]> output;
+  sc_core::sc_out<uint32_t> rk_addr;
+  sc_core::sc_out<bool> done;
+
+  // State logic
+  sc_core::sc_signal<aes_state> sreg;
+  sc_core::sc_signal<aes_state> snext;
+
+  // Internal values
+  sc_core::sc_signal<uint32_t> x0, x1, x2, x3, y0, y1, y2, y3;
+  int round;
+
+  // Modules
+  AES_FROUND *fround;
+  sc_core::sc_signal<uint32_t> froundin0, froundin1, froundin2, froundin3, froundout0, froundout1, froundout2, froundout3;
+  AES_FSb *fsb;
+  sc_core::sc_signal<uint32_t> fsbin0, fsbin1, fsbin2, fsbin3, fsbout0, fsbout1, fsbout2, fsbout3;
+
+  void get_next_state();
+  void set_state();
+
+  SC_CTOR(AES_encrypt)
+  {
+    round = 0;
+
+    fround = new AES_FROUND("fround");
+    fround->y0(froundin0); fround->y1(froundin1); fround->y2(froundin2); fround->y3(froundin3);
+    fround->rk0(rk0); fround->rk1(rk1); fround->rk2(rk2); fround->rk3(rk3);
+    fround->x0(froundout0); fround->x1(froundout1); fround->x2(froundout2); fround->x3(froundout3);
+
+    fsb = new AES_FSb("fsb");
+    fsb->y0(fsbin0); fsb->y1(fsbin1); fsb->y2(fsbin2); fsb->y3(fsbin3);
+    fsb->rk0(rk0); fsb->rk1(rk1); fsb->rk2(rk2); fsb->rk3(rk3);
+    fsb->x0(fsbout0); fsb->x1(fsbout1); fsb->x2(fsbout2); fsb->x3(fsbout3);
+
+    SC_METHOD(get_next_state);
+      sensitive << sreg << start << clear << round;
+	SC_METHOD(set_state);
+      sensitive << clock.pos();
+  }
+};
