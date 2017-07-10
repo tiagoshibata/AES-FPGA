019508dc2bd638888d6a19dfb622a89679000d9d
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-07-09 22:23:45 -0300

    Start CTR simulation

--- include/systemc/main.cc
+++ include/systemc/main.cc
@@ -1,39 +1,36 @@
 #include <systemc>
 #include <iostream>
 
-#include "aes_encrypt.h"
+#include "aes_ctr.h"
 
 int sc_main(int argc, char* argv[])
 {
   std::cout << "Starting simulation: " << sc_core::sc_time_stamp() << "\n";
 
   sc_core::sc_time clock_time(1, sc_core::sc_time_unit::SC_MS);
-  AES_Encrypt encrypt("encrypt");
+  AES_CTR ctr("AES_CTR");
 
   sc_core::sc_signal<bool> clock, start, clear;
-  sc_core::sc_signal<uint32_t> rk0, rk1, rk2, rk3;
-  sc_core::sc_vector<sc_core::sc_signal<unsigned char>> input("AES_input", 16), output("AES_input", 16);
-  sc_core::sc_signal<uint32_t> rk_addr;
+  sc_core::sc_vector<sc_core::sc_signal<unsigned char>> input("AES_input", 16),
+      nonce_counter("AES_nonce_counter", 16), key("AES_key", 16);
+  sc_core::sc_vector<sc_core::sc_signal<unsigned char>> output;
   sc_core::sc_signal<bool> done;
 
-  encrypt.clock(clock);
-  encrypt.start(start);
-  encrypt.clear(clear);
-  encrypt.rk0(rk0);
-  encrypt.rk1(rk1);
-  encrypt.rk2(rk2);
-  encrypt.rk3(rk3);
+  ctr.clock(clock);
+  ctr.start(start);
+  ctr.clear(clear);
+  ctr.input(input);
+  ctr.nonce_counter(nonce_counter);
+  ctr.key(key);
+  ctr.output(output);
+  ctr.done(done);
 
-  encrypt.input(input);
-  encrypt.output(output);
-
-  encrypt.rk_addr(rk_addr);
-  encrypt.done(done);
-  encrypt.set_state();
-
-  sc_core::sc_start(clock_time);
-  sc_core::sc_start(clock_time);
-  sc_core::sc_start(clock_time);
+  while (!ctr.done) {
+    std::cout << "Running 1 cycle\n";
+    sc_core::sc_start(clock_time);
+  }
+  // sc_core::sc_start(clock_time);
+  // sc_core::sc_start(clock_time);
 
   std::cout << "Ended simulation: " << sc_core::sc_time_stamp() << "\n";
   return 0;
