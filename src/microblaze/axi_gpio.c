283dbbbb3ed978739dcb48704c2c3cd4f8394099
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-07-30 18:56:02 -0300

    Softcore to AXI bus GPIO communication

---
+++ src/microblaze/axi_gpio.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+
+volatile uint32_t *axi_gpio_base = (uint32_t *)0x40000000;
+#define GPIO_DATA   0
+#define GPIO_TRI    1
+
+char axi_gpio_get_ready_bit() {
+    // Read GPIO ready bit
+    return axi_gpio_base[GPIO_DATA] & (1 << 2);
+}
+
+void axi_gpio_set_clear_bit(char value) {
+    value = !!value;  // constrain to 0 or 1
+    axi_gpio_base[GPIO_DATA] |= value;
+}
+
+void axi_gpio_set_start_bit(char value) {
+    value = !!value;  // constrain to 0 or 1
+    axi_gpio_base[GPIO_DATA] |= (value << 2);
+}
