c9088054513f99205f500377e5e62b9259b461a2
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-07-30 19:24:48 -0300

    Rename ready -> done pin

--- src/microblaze/axi_gpio.c
+++ src/microblaze/axi_gpio.c
@@ -4,8 +4,8 @@ volatile uint32_t *axi_gpio_base = (uint32_t *)0x40000000;
 #define GPIO_DATA   0
 #define GPIO_TRI    1
 
-char axi_gpio_get_ready_bit() {
-    // Read GPIO ready bit
+char axi_gpio_get_done_bit() {
+    // Done GPIO ready bit
     return axi_gpio_base[GPIO_DATA] & (1 << 2);
 }
 
@@ -16,5 +16,5 @@ void axi_gpio_set_clear_bit(char value) {
 
 void axi_gpio_set_start_bit(char value) {
     value = !!value;  // constrain to 0 or 1
-    axi_gpio_base[GPIO_DATA] |= (value << 2);
+    axi_gpio_base[GPIO_DATA] |= (value << 1);
 }
