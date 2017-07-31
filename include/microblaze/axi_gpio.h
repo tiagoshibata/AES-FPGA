b0655f0452734ef28e1369cabb2f56f33069c72a
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-07-31 13:21:12 -0300

    Move AXI GPIO access to header

--- include/microblaze/axi_gpio.h
+++ include/microblaze/axi_gpio.h
@@ -4,17 +4,17 @@ volatile uint32_t *axi_gpio_base = (uint32_t *)0x40000000;
 #define GPIO_DATA   0
 #define GPIO_TRI    1
 
-char axi_gpio_get_done_bit() {
-    // Done GPIO ready bit
+static inline char axi_gpio_get_done_bit() {
+    // Read GPIO done bit
     return axi_gpio_base[GPIO_DATA] & (1 << 2);
 }
 
-void axi_gpio_set_clear_bit(char value) {
+static inline void axi_gpio_set_clear_bit(char value) {
     value = !!value;  // constrain to 0 or 1
     axi_gpio_base[GPIO_DATA] |= value;
 }
 
-void axi_gpio_set_start_bit(char value) {
+static inline void axi_gpio_set_start_bit(char value) {
     value = !!value;  // constrain to 0 or 1
     axi_gpio_base[GPIO_DATA] |= (value << 1);
 }
